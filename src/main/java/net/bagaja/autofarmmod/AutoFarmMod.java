e42061e73cf76224821bd1fbcde81d5eb46ae8c8
niklasT201 <jackvcrackarchive@gmail.com>
2026-03-11 02:02:27 +0100

    updated to 1.21.3

--- src/main/java/net/bagaja/autofarmmod/AutoFarmMod.java
+++ src/main/java/net/bagaja/autofarmmod/AutoFarmMod.java
@@ -1,7 +1,6 @@
 package net.bagaja.autofarmmod;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
@@ -11,7 +10,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
